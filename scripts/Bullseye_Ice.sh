16faa16245ef8f3814ebbf43c29d2a4b37261f95
debian-openbox <77241939+debian-openbox@users.noreply.github.com>
2021-04-15 12:15:20 +0200

    Update Bullseye_Ice.sh

--- scripts/Bullseye_Ice.sh
+++ scripts/Bullseye_Ice.sh
@@ -5,10 +5,10 @@ sudo apt update && sudo apt -y upgrade
 wget https://ice-wm.org/scripts/os-depends.sh
 sudo bash -x ./os-depends.sh
 
-wget https://github.com/ice-wm/icewm/releases/download/2.3.1/icewm-2.3.1.tar.lz
+wget https://github.com/ice-wm/icewm/releases/download/2.3.2/icewm-2.3.2.tar.lz
 sudo apt install lzip -yy
-tar -x --lzip -vpf icewm-2.3.1.tar.lz
-cd icewm-2.3.1
+tar -x --lzip -vpf icewm-2.3.2.tar.lz
+cd icewm-2.3.2
 ./configure --prefix=/usr
 make
 sudo make install
